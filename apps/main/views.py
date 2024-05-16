3ca0dca96c0e291ae4ef7558d7356916c4df1d25
kpei4ni <mkoragin09@gmail.com>
2024-05-16 23:39:58 +0300

    end of first day

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'base.html')
+
+def lol(request):
+    return HttpResponse('<h1>ЖЕКА LOL</h1> <a href="/"></a>')
